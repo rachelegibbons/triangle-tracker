a98a6ad40f18f1b1f64d07cdd6508a58ffb73b32
Rachel Gibbons and Sky Scholfield <rachel@>
2017-02-22 20:10:09 -0800

    delete practice script from scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,3 @@ $(Document).ready(function(){
   $("#output").show();
   });
 });
-
-
-// First iteration
-// var result;
-// if (side1 === side2 && side2 === side3 && side1 === side3){
-//   result = ("#output").show("#equalateral");
-// } else if (side1 === side2 || side1 === side3 || side2 === side3){
-//   result = ("#output").show("#isosceles");
-// } else if (side1 != side2 && side1 != side3 && side2 != side3){
-//   result = ("#output").show("#scalene");
-// }
