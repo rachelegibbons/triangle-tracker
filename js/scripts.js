1948c85b71e47c44c7df8d598147bb558459b392
Rachel Gibbons and Sky Scholfield <rachel@>
2017-02-22 20:47:40 -0800

    update to include resultfor not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,18 @@ $(Document).ready(function(){
   var side3 = parseInt($("#side3").val());
 
   var result;
-  if (side1 === side2 && side2 === side3 && side1 === side3){
+
+  if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1){
+    result = "not a";
+  }
+    else if (side1 === side2 && side2 === side3 && side1 === side3){
     result = "equalateral";
   } else if (side1 === side2 || side1 === side3 || side2 === side3){
     result = "isosceles";
   } else if (side1 != side2 && side1 != side3 && side2 != side3){
     result = "scalene";
+  } else {
+    result = "Not a triangle"
   }
 
   $(".triangle").text(result);
